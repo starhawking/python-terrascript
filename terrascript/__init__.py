d000eb7d8930b5c21a562e1f25c680c044d05602
Nielson Santana <nielsonnas@gmail.com>
2018-05-04 10:01:35 +0000

    Added support to update one terrascript from other terrascript. Same behavior as dict().update

--- terrascript/__init__.py
+++ terrascript/__init__.py
@@ -53,10 +53,10 @@ class _Config(dict):
 class Terrascript(object):
     """Top-level container for Terraform configurations."""
 
-
     def __init__(self):
 
         self.config = _Config()
+        self._item_list = []
 
     def __add__(self, item):
         # Work-around for issue 3 as described in https://github.com/hashicorp/terraform/issues/13037:
@@ -72,13 +72,24 @@ class Terrascript(object):
         else:
             raise KeyError(item)
 
+        if not isinstance(item, Terrascript):
+            if item in self._item_list:
+                self._item_list.remove(item)
+            self._item_list.append(item)
+
         return self
 
-    
     def add(self, item):
         self.__add__(item)
         return item
 
+    def update(self, terrascript2):
+        if isinstance(terrascript2, Terrascript):
+            for item in terrascript2._item_list:
+                self.__add__(item)
+        else:
+            raise TypeError('{0} is not a Terrascript instance.'.format(
+                type(terrascript2)))
 
     def dump(self):
         """Return the JSON representaion of config."""
