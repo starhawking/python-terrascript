4c26037e131d6d5873db427dc03723b01f6276e2
Markus Juenemann <markus@juenemann.net>
2018-05-10 08:21:35 +1000

    Changed test for issue32

--- tests/test_issue32.py
+++ tests/test_issue32.py
@@ -1,21 +1,21 @@
 from terrascript import Terrascript, provisioner, provider
 from terrascript.aws.r import aws_instance
 
+
 def test_issue32():
     """Issue 32: provisioner return one line with provisioner.file instead a dictionary."""
 
     ts = Terrascript()
 
-    ts += provider('aws', region='us-east-1')
+    ts += provider('aws', region='ap-southeast-2')
 
-    p = provisioner('file', source='pgbouncer-config', destination='/tmp/pgbouncer')
+    p = provisioner('local-exec', command='date > $(mktemp tmpXXXXXXX.terrascript)')
 
-    ts += aws_instance('I1', ami='ami-4bf3d731', instance_type='t2.large', provisioner=p)
+    ts += aws_instance('I1', ami='ami-60a26a02', instance_type='t2.nano', provisioner=p)
 
     j = ts.dump()
 
-    assert 'file' in j
-    assert 'pgbouncer' in j
+    assert 'mktemp' in j
 
     assert ts.validate(delete=False) is True
 
