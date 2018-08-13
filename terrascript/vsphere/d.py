e1bc4e89722ae3cdd43e372196842f5b0a1a9b58
austin-millan <austin.millan@protonmail.com>
2018-08-13 13:58:18 -0700

    vsphere data sources/resources

--- terrascript/vsphere/d.py
+++ terrascript/vsphere/d.py
@@ -11,8 +11,14 @@ virtual_machine = vsphere_virtual_machine
 class vsphere_custom_attribute(_data): pass
 custom_attribute = vsphere_custom_attribute
 
+class vsphere_datastore(_data): pass
+datastore = vsphere_datastore
+
 class vsphere_datastore_cluster(_data): pass
 datastore_cluster = vsphere_datastore_cluster
+
+class vsphere_compute_cluster(_data): pass
+compute_cluster = vsphere_compute_cluster
 
 class vsphere_resource_pool(_data): pass
 resource_pool = vsphere_resource_pool
@@ -31,4 +37,3 @@ tag_category = vsphere_tag_category
 
 class vsphere_vmfs_disks(_data): pass
 vmfs_disks = vsphere_vmfs_disks
-
