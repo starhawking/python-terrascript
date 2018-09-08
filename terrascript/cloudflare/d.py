86cbc6f46a81068e8d4f9838b4c4d9cd1eeeda39
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-09-08 18:29:23 -0500

    Added new cloudflare resources and data

--- terrascript/cloudflare/d.py
+++ terrascript/cloudflare/d.py
@@ -1,1 +1,3 @@
 from terrascript import _data
+class cloudflare_ip_ranges(_data): pass
+ip_ranges = cloudflare_ip_ranges
--- terrascript/cloudflare/r.py
+++ terrascript/cloudflare/r.py
@@ -2,3 +2,26 @@ from terrascript import _resource
 class cloudflare_record(_resource): pass
 record = cloudflare_record
 
+class cloudflare_access_rule(_resource): pass
+access_rule = cloudflare_access_rule
+
+class cloudflare_load_balancer(_resource): pass
+load_balancer = cloudflare_load_balancer
+
+class cloudflare_load_balancer_pool(_resource): pass
+load_balancer_pool = cloudflare_load_balancer_pool
+
+class cloudflare_page_rule(_resource): pass
+page_rule = cloudflare_page_rule
+
+class cloudflare_rate_limit(_resource): pass
+rate_limit = cloudflare_rate_limit
+
+class cloudflare_record(_resource): pass
+record = cloudflare_record
+
+class cloudflare_load_balancer_monitor(_resource): pass
+load_balancer_monitor = cloudflare_load_balancer_monitor
+
+class cloudflare_zone_settings_override(_resource): pass
+zone_settings_override = cloudflare_zone_settings_override
