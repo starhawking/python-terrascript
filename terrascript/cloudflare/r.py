f6220c9f42f9a05b551eec883aec4b7dc25be5ce
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-09-27 00:58:45 -0500

    Add Worker to cloudflare

--- terrascript/cloudflare/r.py
+++ terrascript/cloudflare/r.py
@@ -1,12 +1,13 @@
 from terrascript import _resource
-class cloudflare_record(_resource): pass
-record = cloudflare_record
 
 class cloudflare_access_rule(_resource): pass
 access_rule = cloudflare_access_rule
 
 class cloudflare_load_balancer(_resource): pass
 load_balancer = cloudflare_load_balancer
+
+class cloudflare_load_balancer_monitor(_resource): pass
+load_balancer_monitor = cloudflare_load_balancer_monitor
 
 class cloudflare_load_balancer_pool(_resource): pass
 load_balancer_pool = cloudflare_load_balancer_pool
@@ -20,8 +21,14 @@ rate_limit = cloudflare_rate_limit
 class cloudflare_record(_resource): pass
 record = cloudflare_record
 
-class cloudflare_load_balancer_monitor(_resource): pass
-load_balancer_monitor = cloudflare_load_balancer_monitor
+class cloudflare_worker_route(_resource): pass
+worker_route = cloudflare_worker_route
+
+class cloudflare_worker_script: pass
+worker_script = cloudflare_worker_script
+
+class cloudflare_zone_lockdown: pass
+zone_lockdown = cloudflare_zone_lockdown
 
 class cloudflare_zone_settings_override(_resource): pass
 zone_settings_override = cloudflare_zone_settings_override
