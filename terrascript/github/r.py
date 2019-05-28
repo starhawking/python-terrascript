835540c38c6f9409afcd560de26a07ba11794cc9
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:39 +0100

    Update github provider

--- terrascript/github/r.py
+++ terrascript/github/r.py
@@ -1,6 +1,39 @@
 from terrascript import _resource
-class github_team(_resource): pass
-team = github_team
+class github_branch_protection(_resource): pass
+branch_protection = github_branch_protection
+
+class github_issue_label(_resource): pass
+issue_label = github_issue_label
+
+class github_membership(_resource): pass
+membership = github_membership
+
+class github_organization_block(_resource): pass
+organization_block = github_organization_block
+
+class github_organization_project(_resource): pass
+organization_project = github_organization_project
+
+class github_organization_webhook(_resource): pass
+organization_webhook = github_organization_webhook
+
+class github_project_column(_resource): pass
+project_column = github_project_column
+
+class github_repository_collaborator(_resource): pass
+repository_collaborator = github_repository_collaborator
+
+class github_repository_deploy_key(_resource): pass
+repository_deploy_key = github_repository_deploy_key
+
+class github_repository_project(_resource): pass
+repository_project = github_repository_project
+
+class github_repository_webhook(_resource): pass
+repository_webhook = github_repository_webhook
+
+class github_repository(_resource): pass
+repository = github_repository
 
 class github_team_membership(_resource): pass
 team_membership = github_team_membership
@@ -8,27 +41,15 @@ team_membership = github_team_membership
 class github_team_repository(_resource): pass
 team_repository = github_team_repository
 
-class github_membership(_resource): pass
-membership = github_membership
+class github_team(_resource): pass
+team = github_team
 
-class github_repository(_resource): pass
-repository = github_repository
+class github_user_gpg_key(_resource): pass
+user_gpg_key = github_user_gpg_key
 
-class github_repository_deploy_key(_resource): pass
-repository_deploy_key = github_repository_deploy_key
+class github_user_invitation_accepter(_resource): pass
+user_invitation_accepter = github_user_invitation_accepter
 
-class github_repository_webhook(_resource): pass
-repository_webhook = github_repository_webhook
-
-class github_organization_webhook(_resource): pass
-organization_webhook = github_organization_webhook
-
-class github_repository_collaborator(_resource): pass
-repository_collaborator = github_repository_collaborator
-
-class github_issue_label(_resource): pass
-issue_label = github_issue_label
-
-class github_branch_protection(_resource): pass
-branch_protection = github_branch_protection
+class github_user_ssh_key(_resource): pass
+user_ssh_key = github_user_ssh_key
 
