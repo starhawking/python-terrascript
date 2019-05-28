039c8952392e15215435fb1eace57e9c241b1d26
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:31 +0100

    Update dns provider

--- terrascript/dns/r.py
+++ terrascript/dns/r.py
@@ -2,15 +2,21 @@ from terrascript import _resource
 class dns_a_record_set(_resource): pass
 a_record_set = dns_a_record_set
 
-class dns_ns_record_set(_resource): pass
-ns_record_set = dns_ns_record_set
-
 class dns_aaaa_record_set(_resource): pass
 aaaa_record_set = dns_aaaa_record_set
 
 class dns_cname_record(_resource): pass
 cname_record = dns_cname_record
 
+class dns_mx_record_set(_resource): pass
+mx_record_set = dns_mx_record_set
+
+class dns_ns_record_set(_resource): pass
+ns_record_set = dns_ns_record_set
+
 class dns_ptr_record(_resource): pass
 ptr_record = dns_ptr_record
+
+class dns_txt_record_set(_resource): pass
+txt_record_set = dns_txt_record_set
 
