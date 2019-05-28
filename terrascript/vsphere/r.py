bdb0129a260d0fa45c79fa28c4effce6f2f5eeec
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:42 +0100

    Update vsphere provider

--- terrascript/vsphere/r.py
+++ terrascript/vsphere/r.py
@@ -1,22 +1,9 @@
 from terrascript import _resource
-class vsphere_custom_attribute(_resource): pass
-custom_attribute = vsphere_custom_attribute
-
-class vsphere_datastore_cluster(_resource): pass
-datastore_cluster = vsphere_datastore_cluster
-
-class vsphere_resource_pool(_resource): pass
-resource_pool = vsphere_resource_pool
-
-class vsphere_storage_drs_vm_override(_resource): pass
-storage_drs_vm_override = vsphere_storage_drs_vm_override
-
-class vsphere_datacenter(_resource): pass
-datacenter = vsphere_datacenter
-
-
 class vsphere_compute_cluster(_resource): pass
-compute_cluster= vsphere_compute_cluster
+compute_cluster = vsphere_compute_cluster
+
+class vsphere_compute_cluster_host_group(_resource): pass
+compute_cluster_host_group = vsphere_compute_cluster_host_group
 
 class vsphere_compute_cluster_vm_affinity_rule(_resource): pass
 compute_cluster_vm_affinity_rule = vsphere_compute_cluster_vm_affinity_rule
@@ -33,17 +20,14 @@ compute_cluster_vm_group = vsphere_compute_cluster_vm_group
 class vsphere_compute_cluster_vm_host_rule(_resource): pass
 compute_cluster_vm_host_rule = vsphere_compute_cluster_vm_host_rule
 
-class vsphere_drs_vm_override(_resource): pass
-drs_vm_override = vsphere_drs_vm_override
+class vsphere_custom_attribute(_resource): pass
+custom_attribute = vsphere_custom_attribute
 
-class vsphere_dpm_host_override(_resource): pass
-dpm_host_override = vsphere_dpm_host_override
+class vsphere_datacenter(_resource): pass
+datacenter = vsphere_datacenter
 
-class vsphere_ha_vm_override(_resource): pass
-ha_vm_override = vsphere_ha_vm_override
-
-class vsphere_compute_cluster_host_group(_resource): pass
-compute_cluster_host_group = vsphere_compute_cluster_host_group
+class vsphere_datastore_cluster(_resource): pass
+datastore_cluster = vsphere_datastore_cluster
 
 class vsphere_datastore_cluster_vm_anti_affinity_rule(_resource): pass
 datastore_cluster_vm_anti_affinity_rule = vsphere_datastore_cluster_vm_anti_affinity_rule
@@ -54,11 +38,20 @@ distributed_port_group = vsphere_distributed_port_group
 class vsphere_distributed_virtual_switch(_resource): pass
 distributed_virtual_switch = vsphere_distributed_virtual_switch
 
+class vsphere_drs_vm_override(_resource): pass
+drs_vm_override = vsphere_drs_vm_override
+
+class vsphere_dpm_host_override(_resource): pass
+dpm_host_override = vsphere_dpm_host_override
+
 class vsphere_file(_resource): pass
 file = vsphere_file
 
 class vsphere_folder(_resource): pass
 folder = vsphere_folder
+
+class vsphere_ha_vm_override(_resource): pass
+ha_vm_override = vsphere_ha_vm_override
 
 class vsphere_host_port_group(_resource): pass
 host_port_group = vsphere_host_port_group
@@ -68,6 +61,9 @@ host_virtual_switch = vsphere_host_virtual_switch
 
 class vsphere_license(_resource): pass
 license = vsphere_license
+
+class vsphere_resource_pool(_resource): pass
+resource_pool = vsphere_resource_pool
 
 class vsphere_tag(_resource): pass
 tag = vsphere_tag
@@ -84,8 +80,18 @@ virtual_machine = vsphere_virtual_machine
 class vsphere_nas_datastore(_resource): pass
 nas_datastore = vsphere_nas_datastore
 
+class vsphere_storage_drs_vm_override(_resource): pass
+storage_drs_vm_override = vsphere_storage_drs_vm_override
+
+class vsphere_vapp_container(_resource): pass
+vapp_container = vsphere_vapp_container
+
+class vsphere_vapp_entity(_resource): pass
+vapp_entity = vsphere_vapp_entity
+
 class vsphere_vmfs_datastore(_resource): pass
 vmfs_datastore = vsphere_vmfs_datastore
 
 class vsphere_virtual_machine_snapshot(_resource): pass
 virtual_machine_snapshot = vsphere_virtual_machine_snapshot
+
