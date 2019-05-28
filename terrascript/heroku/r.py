519949baaa22659b7decc8433fa6cc19c58b5547
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:47 +0100

    Update heroku provider

--- terrascript/heroku/r.py
+++ terrascript/heroku/r.py
@@ -1,4 +1,7 @@
 from terrascript import _resource
+class heroku_account_feature(_resource): pass
+account_feature = heroku_account_feature
+
 class heroku_addon(_resource): pass
 addon = heroku_addon
 
@@ -8,11 +11,23 @@ addon_attachment = heroku_addon_attachment
 class heroku_app(_resource): pass
 app = heroku_app
 
+class heroku_app_config_association(_resource): pass
+app_config_association = heroku_app_config_association
+
 class heroku_app_feature(_resource): pass
 app_feature = heroku_app_feature
 
+class heroku_app_release(_resource): pass
+app_release = heroku_app_release
+
+class heroku_build(_resource): pass
+build = heroku_build
+
 class heroku_cert(_resource): pass
 cert = heroku_cert
+
+class heroku_config(_resource): pass
+config = heroku_config
 
 class heroku_domain(_resource): pass
 domain = heroku_domain
@@ -20,12 +35,36 @@ domain = heroku_domain
 class heroku_drain(_resource): pass
 drain = heroku_drain
 
+class heroku_formation(_resource): pass
+formation = heroku_formation
+
 class heroku_pipeline(_resource): pass
 pipeline = heroku_pipeline
 
 class heroku_pipeline_coupling(_resource): pass
 pipeline_coupling = heroku_pipeline_coupling
 
+class heroku_slug(_resource): pass
+slug = heroku_slug
+
 class heroku_space(_resource): pass
 space = heroku_space
+
+class heroku_space_inbound_ruleset(_resource): pass
+space_inbound_ruleset = heroku_space_inbound_ruleset
+
+class heroku_space_app_access(_resource): pass
+space_app_access = heroku_space_app_access
+
+class heroku_space_peering_connection_accepter(_resource): pass
+space_peering_connection_accepter = heroku_space_peering_connection_accepter
+
+class heroku_space_vpn_connection(_resource): pass
+space_vpn_connection = heroku_space_vpn_connection
+
+class heroku_team_collaborator(_resource): pass
+team_collaborator = heroku_team_collaborator
+
+class heroku_team_member(_resource): pass
+team_member = heroku_team_member
 
