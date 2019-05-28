174ac31378f515d19bd9c56c711b636ee151da15
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:16 +0100

    Update postgresql provider

--- terrascript/postgresql/r.py
+++ terrascript/postgresql/r.py
@@ -2,8 +2,14 @@ from terrascript import _resource
 class postgresql_database(_resource): pass
 database = postgresql_database
 
+class postgresql_default_privileges(_resource): pass
+default_privileges = postgresql_default_privileges
+
 class postgresql_extension(_resource): pass
 extension = postgresql_extension
+
+class postgresql_grant(_resource): pass
+grant = postgresql_grant
 
 class postgresql_schema(_resource): pass
 schema = postgresql_schema
