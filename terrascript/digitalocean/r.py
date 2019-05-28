406e65d4327190a500dbe73bc6667a73d61bee80
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:28 +0100

    Update digitalocean provider

--- terrascript/digitalocean/r.py
+++ terrascript/digitalocean/r.py
@@ -2,11 +2,20 @@ from terrascript import _resource
 class digitalocean_certificate(_resource): pass
 certificate = digitalocean_certificate
 
+class digitalocean_cdn(_resource): pass
+cdn = digitalocean_cdn
+
+class digitalocean_database_cluster(_resource): pass
+database_cluster = digitalocean_database_cluster
+
 class digitalocean_domain(_resource): pass
 domain = digitalocean_domain
 
 class digitalocean_droplet(_resource): pass
 droplet = digitalocean_droplet
+
+class digitalocean_droplet_snapshot(_resource): pass
+droplet_snapshot = digitalocean_droplet_snapshot
 
 class digitalocean_firewall(_resource): pass
 firewall = digitalocean_firewall
@@ -14,11 +23,26 @@ firewall = digitalocean_firewall
 class digitalocean_floating_ip(_resource): pass
 floating_ip = digitalocean_floating_ip
 
+class digitalocean_floating_ip_assignment(_resource): pass
+floating_ip_assignment = digitalocean_floating_ip_assignment
+
+class digitalocean_kubernetes_cluster(_resource): pass
+kubernetes_cluster = digitalocean_kubernetes_cluster
+
+class digitalocean_kubernetes_node_pool(_resource): pass
+kubernetes_node_pool = digitalocean_kubernetes_node_pool
+
 class digitalocean_loadbalancer(_resource): pass
 loadbalancer = digitalocean_loadbalancer
 
+class digitalocean_project(_resource): pass
+project = digitalocean_project
+
 class digitalocean_record(_resource): pass
 record = digitalocean_record
+
+class digitalocean_spaces_bucket(_resource): pass
+spaces_bucket = digitalocean_spaces_bucket
 
 class digitalocean_ssh_key(_resource): pass
 ssh_key = digitalocean_ssh_key
@@ -34,3 +58,4 @@ volume_attachment = digitalocean_volume_attachment
 
 class digitalocean_volume_snapshot(_resource): pass
 volume_snapshot = digitalocean_volume_snapshot
+
