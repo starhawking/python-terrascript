5f4c55e08f6ee6352bfc40d1da6fdc5ec5021fe8
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:12 +0100

    Update ovh provider

--- terrascript/ovh/d.py
+++ terrascript/ovh/d.py
@@ -1,7 +1,19 @@
 from terrascript import _data
-class ovh_publiccloud_region(_data): pass
-publiccloud_region = ovh_publiccloud_region
+class ovh_cloud_region(_data): pass
+cloud_region = ovh_cloud_region
 
-class ovh_publiccloud_regions(_data): pass
-publiccloud_regions = ovh_publiccloud_regions
+class ovh_cloud_regions(_data): pass
+cloud_regions = ovh_cloud_regions
+
+class ovh_domain_zone(_data): pass
+domain_zone = ovh_domain_zone
+
+class ovh_iploadbalancing(_data): pass
+iploadbalancing = ovh_iploadbalancing
+
+class ovh_me_paymentmean_bankaccount(_data): pass
+me_paymentmean_bankaccount = ovh_me_paymentmean_bankaccount
+
+class ovh_me_paymentmean_creditcard(_data): pass
+me_paymentmean_creditcard = ovh_me_paymentmean_creditcard
 
--- terrascript/ovh/r.py
+++ terrascript/ovh/r.py
@@ -1,13 +1,40 @@
 from terrascript import _resource
-class ovh_publiccloud_private_network(_resource): pass
-publiccloud_private_network = ovh_publiccloud_private_network
+class ovh_iploadbalancing_tcp_farm(_resource): pass
+iploadbalancing_tcp_farm = ovh_iploadbalancing_tcp_farm
 
-class ovh_publiccloud_private_network_subnet(_resource): pass
-publiccloud_private_network_subnet = ovh_publiccloud_private_network_subnet
+class ovh_iploadbalancing_tcp_farm_server(_resource): pass
+iploadbalancing_tcp_farm_server = ovh_iploadbalancing_tcp_farm_server
 
-class ovh_publiccloud_user(_resource): pass
-publiccloud_user = ovh_publiccloud_user
+class ovh_iploadbalancing_tcp_frontend(_resource): pass
+iploadbalancing_tcp_frontend = ovh_iploadbalancing_tcp_frontend
 
-class ovh_vrack_publiccloud_attachment(_resource): pass
-vrack_publiccloud_attachment = ovh_vrack_publiccloud_attachment
+class ovh_iploadbalancing_http_route(_resource): pass
+iploadbalancing_http_route = ovh_iploadbalancing_http_route
+
+class ovh_iploadbalancing_http_route_rule(_resource): pass
+iploadbalancing_http_route_rule = ovh_iploadbalancing_http_route_rule
+
+class ovh_iploadbalancing_refresh(_resource): pass
+iploadbalancing_refresh = ovh_iploadbalancing_refresh
+
+class ovh_domain_zone_record(_resource): pass
+domain_zone_record = ovh_domain_zone_record
+
+class ovh_domain_zone_redirection(_resource): pass
+domain_zone_redirection = ovh_domain_zone_redirection
+
+class ovh_ip_reverse(_resource): pass
+ip_reverse = ovh_ip_reverse
+
+class ovh_cloud_network_private(_resource): pass
+cloud_network_private = ovh_cloud_network_private
+
+class ovh_cloud_network_private_subnet(_resource): pass
+cloud_network_private_subnet = ovh_cloud_network_private_subnet
+
+class ovh_cloud_user(_resource): pass
+cloud_user = ovh_cloud_user
+
+class ovh_vrack_cloudproject(_resource): pass
+vrack_cloudproject = ovh_vrack_cloudproject
 
