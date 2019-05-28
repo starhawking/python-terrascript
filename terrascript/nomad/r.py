d96023f24087cf18b483b5b7f5eb27e6ac01dc22
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:04 +0100

    Update nomad provider

--- terrascript/nomad/r.py
+++ terrascript/nomad/r.py
@@ -1,4 +1,19 @@
 from terrascript import _resource
+class nomad_acl_policy(_resource): pass
+acl_policy = nomad_acl_policy
+
+class nomad_acl_token(_resource): pass
+acl_token = nomad_acl_token
+
 class nomad_job(_resource): pass
 job = nomad_job
+
+class nomad_namespace(_resource): pass
+namespace = nomad_namespace
+
+class nomad_quota_specification(_resource): pass
+quota_specification = nomad_quota_specification
+
+class nomad_sentinel_policy(_resource): pass
+sentinel_policy = nomad_sentinel_policy
 
