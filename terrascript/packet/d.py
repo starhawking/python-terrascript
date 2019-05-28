6283ab6cb3236bf6082ef00f6343e59efff17602
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:14 +0100

    Update packet provider

--- terrascript/packet/d.py
+++ terrascript/packet/d.py
@@ -2,3 +2,9 @@ from terrascript import _data
 class packet_precreated_ip_block(_data): pass
 precreated_ip_block = packet_precreated_ip_block
 
+class packet_operating_system(_data): pass
+operating_system = packet_operating_system
+
+class packet_spot_market_price(_data): pass
+spot_market_price = packet_spot_market_price
+
