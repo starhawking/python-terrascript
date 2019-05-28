38f6587111f982a21dfe2009a599a8488cd987dc
Ryan Walder <ryan@tfu.io>
2019-05-28 10:56:03 +0100

    Update newrelic provider

--- terrascript/newrelic/r.py
+++ terrascript/newrelic/r.py
@@ -5,12 +5,27 @@ alert_channel = newrelic_alert_channel
 class newrelic_alert_condition(_resource): pass
 alert_condition = newrelic_alert_condition
 
-class newrelic_nrql_alert_condition(_resource): pass
-nrql_alert_condition = newrelic_nrql_alert_condition
+class newrelic_alert_policy_channel(_resource): pass
+alert_policy_channel = newrelic_alert_policy_channel
 
 class newrelic_alert_policy(_resource): pass
 alert_policy = newrelic_alert_policy
 
-class newrelic_alert_policy_channel(_resource): pass
-alert_policy_channel = newrelic_alert_policy_channel
+class newrelic_dashboard(_resource): pass
+dashboard = newrelic_dashboard
+
+class newrelic_infra_alert_condition(_resource): pass
+infra_alert_condition = newrelic_infra_alert_condition
+
+class newrelic_nrql_alert_condition(_resource): pass
+nrql_alert_condition = newrelic_nrql_alert_condition
+
+class newrelic_synthetics_alert_condition(_resource): pass
+synthetics_alert_condition = newrelic_synthetics_alert_condition
+
+class newrelic_synthetics_monitor(_resource): pass
+synthetics_monitor = newrelic_synthetics_monitor
+
+class newrelic_synthetics_monitor_script(_resource): pass
+synthetics_monitor_script = newrelic_synthetics_monitor_script
 
