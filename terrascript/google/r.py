7e54ab57b27d1414ec2990fc0063cc93c667f022
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:41 +0100

    Update google provider

--- terrascript/google/r.py
+++ terrascript/google/r.py
@@ -1,4 +1,7 @@
 from terrascript import _resource
+class google_app_engine_application(_resource): pass
+app_engine_application = google_app_engine_application
+
 class google_bigquery_dataset(_resource): pass
 bigquery_dataset = google_bigquery_dataset
 
@@ -11,50 +14,23 @@ bigtable_instance = google_bigtable_instance
 class google_bigtable_table(_resource): pass
 bigtable_table = google_bigtable_table
 
-class google_compute_autoscaler(_resource): pass
-compute_autoscaler = google_compute_autoscaler
+class google_cloudfunctions_function(_resource): pass
+cloudfunctions_function = google_cloudfunctions_function
 
-class google_compute_address(_resource): pass
-compute_address = google_compute_address
+class google_cloudiot_registry(_resource): pass
+cloudiot_registry = google_cloudiot_registry
 
-class google_compute_backend_bucket(_resource): pass
-compute_backend_bucket = google_compute_backend_bucket
+class google_composer_environment(_resource): pass
+composer_environment = google_composer_environment
 
-class google_compute_backend_service(_resource): pass
-compute_backend_service = google_compute_backend_service
-
-class google_compute_disk(_resource): pass
-compute_disk = google_compute_disk
-
-class google_compute_snapshot(_resource): pass
-compute_snapshot = google_compute_snapshot
-
-class google_compute_firewall(_resource): pass
-compute_firewall = google_compute_firewall
-
-class google_compute_forwarding_rule(_resource): pass
-compute_forwarding_rule = google_compute_forwarding_rule
-
-class google_compute_global_address(_resource): pass
-compute_global_address = google_compute_global_address
-
-class google_compute_global_forwarding_rule(_resource): pass
-compute_global_forwarding_rule = google_compute_global_forwarding_rule
-
-class google_compute_health_check(_resource): pass
-compute_health_check = google_compute_health_check
-
-class google_compute_http_health_check(_resource): pass
-compute_http_health_check = google_compute_http_health_check
-
-class google_compute_https_health_check(_resource): pass
-compute_https_health_check = google_compute_https_health_check
-
-class google_compute_image(_resource): pass
-compute_image = google_compute_image
+class google_compute_attached_disk(_resource): pass
+compute_attached_disk = google_compute_attached_disk
 
 class google_compute_instance(_resource): pass
 compute_instance = google_compute_instance
+
+class google_compute_instance_from_template(_resource): pass
+compute_instance_from_template = google_compute_instance_from_template
 
 class google_compute_instance_group(_resource): pass
 compute_instance_group = google_compute_instance_group
@@ -65,9 +41,6 @@ compute_instance_group_manager = google_compute_instance_group_manager
 class google_compute_instance_template(_resource): pass
 compute_instance_template = google_compute_instance_template
 
-class google_compute_network(_resource): pass
-compute_network = google_compute_network
-
 class google_compute_network_peering(_resource): pass
 compute_network_peering = google_compute_network_peering
 
@@ -77,26 +50,20 @@ compute_project_metadata = google_compute_project_metadata
 class google_compute_project_metadata_item(_resource): pass
 compute_project_metadata_item = google_compute_project_metadata_item
 
-class google_compute_region_autoscaler(_resource): pass
-compute_region_autoscaler = google_compute_region_autoscaler
-
-class google_compute_region_backend_service(_resource): pass
-compute_region_backend_service = google_compute_region_backend_service
-
 class google_compute_region_instance_group_manager(_resource): pass
 compute_region_instance_group_manager = google_compute_region_instance_group_manager
-
-class google_compute_route(_resource): pass
-compute_route = google_compute_route
-
-class google_compute_router(_resource): pass
-compute_router = google_compute_router
 
 class google_compute_router_interface(_resource): pass
 compute_router_interface = google_compute_router_interface
 
+class google_compute_router_nat(_resource): pass
+compute_router_nat = google_compute_router_nat
+
 class google_compute_router_peer(_resource): pass
 compute_router_peer = google_compute_router_peer
+
+class google_compute_security_policy(_resource): pass
+compute_security_policy = google_compute_security_policy
 
 class google_compute_shared_vpc_host_project(_resource): pass
 compute_shared_vpc_host_project = google_compute_shared_vpc_host_project
@@ -104,35 +71,8 @@ compute_shared_vpc_host_project = google_compute_shared_vpc_host_project
 class google_compute_shared_vpc_service_project(_resource): pass
 compute_shared_vpc_service_project = google_compute_shared_vpc_service_project
 
-class google_compute_ssl_certificate(_resource): pass
-compute_ssl_certificate = google_compute_ssl_certificate
-
-class google_compute_subnetwork(_resource): pass
-compute_subnetwork = google_compute_subnetwork
-
-class google_compute_target_http_proxy(_resource): pass
-compute_target_http_proxy = google_compute_target_http_proxy
-
-class google_compute_target_https_proxy(_resource): pass
-compute_target_https_proxy = google_compute_target_https_proxy
-
-class google_compute_target_tcp_proxy(_resource): pass
-compute_target_tcp_proxy = google_compute_target_tcp_proxy
-
-class google_compute_target_ssl_proxy(_resource): pass
-compute_target_ssl_proxy = google_compute_target_ssl_proxy
-
 class google_compute_target_pool(_resource): pass
 compute_target_pool = google_compute_target_pool
-
-class google_compute_url_map(_resource): pass
-compute_url_map = google_compute_url_map
-
-class google_compute_vpn_gateway(_resource): pass
-compute_vpn_gateway = google_compute_vpn_gateway
-
-class google_compute_vpn_tunnel(_resource): pass
-compute_vpn_tunnel = google_compute_vpn_tunnel
 
 class google_container_cluster(_resource): pass
 container_cluster = google_container_cluster
@@ -140,17 +80,20 @@ container_cluster = google_container_cluster
 class google_container_node_pool(_resource): pass
 container_node_pool = google_container_node_pool
 
+class google_dataflow_job(_resource): pass
+dataflow_job = google_dataflow_job
+
 class google_dataproc_cluster(_resource): pass
 dataproc_cluster = google_dataproc_cluster
 
 class google_dataproc_job(_resource): pass
 dataproc_job = google_dataproc_job
 
-class google_dns_managed_zone(_resource): pass
-dns_managed_zone = google_dns_managed_zone
-
 class google_dns_record_set(_resource): pass
 dns_record_set = google_dns_record_set
+
+class google_endpoints_service(_resource): pass
+endpoints_service = google_endpoints_service
 
 class google_folder(_resource): pass
 folder = google_folder
@@ -160,6 +103,12 @@ folder_organization_policy = google_folder_organization_policy
 
 class google_logging_billing_account_sink(_resource): pass
 logging_billing_account_sink = google_logging_billing_account_sink
+
+class google_logging_metric(_resource): pass
+logging_metric = google_logging_metric
+
+class google_logging_organization_sink(_resource): pass
+logging_organization_sink = google_logging_organization_sink
 
 class google_logging_folder_sink(_resource): pass
 logging_folder_sink = google_logging_folder_sink
@@ -173,20 +122,11 @@ kms_key_ring = google_kms_key_ring
 class google_kms_crypto_key(_resource): pass
 kms_crypto_key = google_kms_crypto_key
 
-class google_sourcerepo_repository(_resource): pass
-sourcerepo_repository = google_sourcerepo_repository
-
-class google_spanner_instance(_resource): pass
-spanner_instance = google_spanner_instance
-
-class google_spanner_database(_resource): pass
-spanner_database = google_spanner_database
-
-class google_sql_database(_resource): pass
-sql_database = google_sql_database
-
 class google_sql_database_instance(_resource): pass
 sql_database_instance = google_sql_database_instance
+
+class google_sql_ssl_cert(_resource): pass
+sql_ssl_cert = google_sql_ssl_cert
 
 class google_sql_user(_resource): pass
 sql_user = google_sql_user
@@ -209,14 +149,14 @@ project_service = google_project_service
 class google_project_iam_custom_role(_resource): pass
 project_iam_custom_role = google_project_iam_custom_role
 
+class google_project_organization_policy(_resource): pass
+project_organization_policy = google_project_organization_policy
+
+class google_project_usage_export_bucket(_resource): pass
+project_usage_export_bucket = google_project_usage_export_bucket
+
 class google_project_services(_resource): pass
 project_services = google_project_services
-
-class google_pubsub_topic(_resource): pass
-pubsub_topic = google_pubsub_topic
-
-class google_pubsub_subscription(_resource): pass
-pubsub_subscription = google_pubsub_subscription
 
 class google_runtimeconfig_config(_resource): pass
 runtimeconfig_config = google_runtimeconfig_config
@@ -241,4 +181,13 @@ storage_bucket_object = google_storage_bucket_object
 
 class google_storage_object_acl(_resource): pass
 storage_object_acl = google_storage_object_acl
+
+class google_storage_default_object_acl(_resource): pass
+storage_default_object_acl = google_storage_default_object_acl
+
+class google_storage_notification(_resource): pass
+storage_notification = google_storage_notification
+
+class google_storage_transfer_job(_resource): pass
+storage_transfer_job = google_storage_transfer_job
 
