9c98eade24b497a2b9132b4e473da45599673007
Ryan Walder <ryan@tfu.io>
2019-05-28 10:55:40 +0100

    Update gitlab provider

--- terrascript/gitlab/r.py
+++ terrascript/gitlab/r.py
@@ -1,4 +1,10 @@
 from terrascript import _resource
+class gitlab_branch_protection(_resource): pass
+branch_protection = gitlab_branch_protection
+
+class gitlab_tag_protection(_resource): pass
+tag_protection = gitlab_tag_protection
+
 class gitlab_group(_resource): pass
 group = gitlab_group
 
@@ -8,6 +14,9 @@ project = gitlab_project
 class gitlab_label(_resource): pass
 label = gitlab_label
 
+class gitlab_pipeline_trigger(_resource): pass
+pipeline_trigger = gitlab_pipeline_trigger
+
 class gitlab_project_hook(_resource): pass
 project_hook = gitlab_project_hook
 
@@ -16,4 +25,22 @@ deploy_key = gitlab_deploy_key
 
 class gitlab_user(_resource): pass
 user = gitlab_user
+
+class gitlab_project_membership(_resource): pass
+project_membership = gitlab_project_membership
+
+class gitlab_group_membership(_resource): pass
+group_membership = gitlab_group_membership
+
+class gitlab_project_variable(_resource): pass
+project_variable = gitlab_project_variable
+
+class gitlab_group_variable(_resource): pass
+group_variable = gitlab_group_variable
+
+class gitlab_project_cluster(_resource): pass
+project_cluster = gitlab_project_cluster
+
+class gitlab_service_slack(_resource): pass
+service_slack = gitlab_service_slack
 
