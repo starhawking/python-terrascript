0493e613bc18c1554f0140bfac50e9330b6e191d
Peijun Ma <peijun.ma@protonmail.com>
2019-08-02 11:30:03 -0400

    Make test import sanity compatiable with old Py

--- tests/test_01_import_sanity.py
+++ tests/test_01_import_sanity.py
@@ -6,7 +6,7 @@ from importlib import import_module
 
 
 def test_sanity():
-    for module_info in pkgutil.iter_modules(terrascript.__path__):
-        if module_info.ispkg:
-            import_module(".d", "terrascript." + module_info.name)
-            import_module(".r", "terrascript." + module_info.name)
+    for importer, modname, ispkg in pkgutil.iter_modules(terrascript.__path__):
+        if ispkg:
+            import_module(".d", "terrascript." + modname)
+            import_module(".r", "terrascript." + modname)
