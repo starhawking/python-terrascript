a18335ac7010e096aa8de09f05648caa14b26198
Peijun Ma <peijun.ma@protonmail.com>
2019-08-02 11:02:16 -0400

    Generate new style terrascript classes
    - makecode.py will generate correct new style terrascript classes
    - Handle generated terraform provider resources that are not in
      provider.go
    - Also add special case handling for google-beta provider due to it
      sharing the same "google" prefixes with google provider

--- makecode.py
+++ makecode.py
@@ -32,10 +32,10 @@ if not os.path.isdir(os.path.join(thisdir, PREFIX)):
     print('Script must be run from python-terrascript folder')
     sys.exit(1)
 
-
 gh = github3.login(token=TOKEN)
 for provider in PROVIDERS:
     print(provider)
+    provider_snake_case = provider.replace('-', '_')
     reponame = 'terraform-provider-{}'.format(provider)
     repo = gh.repository(REPO_OWNER, reponame)
     branch = repo.branch(repo.default_branch)
@@ -44,14 +44,17 @@ for provider in PROVIDERS:
 
     regex = re.compile('^'+provider+'/provider.go')
 
-    provider_go_hash = [h for h in tree.recurse().tree
+    recursed_tree = tree.recurse().tree
+    provider_go_hash = [h for h in recursed_tree
                         if regex.match(h.path)][0]
+    generated_go_hashes = [h for h in recursed_tree if h.path.endswith("_gen.go") and h.path.startswith(provider + '/')]
 
     provider_go_blob = repo.blob(provider_go_hash.sha)
-    decoded = base64.b64decode(provider_go_blob.content)
+    generated_go_blobs = [repo.blob(hash_.sha) for hash_ in generated_go_hashes]
+    decoded = base64.b64decode(provider_go_blob.content) + b''.join(base64.b64decode(g.content) for g in generated_go_blobs)
 
-    provider_path = os.path.join(thisdir, PREFIX, provider)
-    provider_init_path = os.path.join(thisdir, PREFIX, provider, '__init__.py')
+    provider_path = os.path.join(thisdir, PREFIX, provider_snake_case)
+    provider_init_path = os.path.join(thisdir, PREFIX, provider_snake_case, '__init__.py')
     resource_path = os.path.join(provider_path, 'r.py')
     data_path = os.path.join(provider_path, 'd.py')
 
@@ -62,19 +65,25 @@ for provider in PROVIDERS:
         fp.write('"""' + time.strftime('%Y-%m-%d %H:%M:%S') + '"""' + '\n')
 
     with open(resource_path, 'w') as fpr:
-        fpr.write('from terrascript import _resource\n')
+        fpr.write('from terrascript import Resource\n')
 
         with open(data_path, 'w') as fpd:
-            fpd.write('from terrascript import _data\n')
+            fpd.write('from terrascript import Data\n')
 
             for m in REGEX.finditer(decoded):
                 type_ = m.group('type_').decode('utf-8')
                 class_ = m.group('class_').decode('utf-8')
 
-                if class_ == 'resource':
-                    fpr.write('class {}_{}(_resource): pass\n'.format(provider, type_))
-                    fpr.write('{} = {}_{}\n\n'.format(type_, provider, type_))
+                if provider == 'google-beta':
+                    block_name = 'google_{}'.format(type_)
                 else:
-                    fpd.write('class {}_{}(_data): pass\n'.format(provider, type_))
-                    fpd.write('{} = {}_{}\n\n'.format(type_, provider, type_))
+                    block_name = '{}_{}'.format(provider_snake_case, type_)
 
+                if class_ == 'resource':
+                    fpr.write('class {}(Resource):\n'.format(block_name))
+                    fpr.write("    def __init__(self, _label, **kwargs): super().__init__('{}', _label, **kwargs)\n".format(block_name))
+                    fpr.write('{} = {}\n\n'.format(type_, block_name))
+                else:
+                    fpd.write('class {}(Data):\n'.format(block_name))
+                    fpd.write("    def __init__(self, _label, **kwargs): super().__init__('{}', _label, **kwargs)\n".format(block_name))
+                    fpd.write('{} = {}\n\n'.format(type_, block_name))
