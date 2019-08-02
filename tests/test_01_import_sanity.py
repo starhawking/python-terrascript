44e0f793717339d577b3330a3cc797c96081d298
Peijun Ma <peijun.ma@protonmail.com>
2019-08-02 11:36:53 -0400

    Skip import tests if Python version < 3.5

--- tests/test_01_import_sanity.py
+++ tests/test_01_import_sanity.py
@@ -1,10 +1,15 @@
 """This test file will import all generated p/r.py code for sanity checking."""
 import pkgutil
+import unittest
+
 import terrascript
 
 from importlib import import_module
 
+import sys
 
+
+@unittest.skipIf(sys.version_info < (3, 5), "import_module doesn't work properly in Python 3.4")
 def test_sanity():
     for importer, modname, ispkg in pkgutil.iter_modules(terrascript.__path__):
         if ispkg:
