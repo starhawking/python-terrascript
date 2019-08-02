992072048d2475943bc68cc5a3f16e57600a7d49
Peijun Ma <peijun.ma@protonmail.com>
2019-08-02 14:27:49 -0400

    Implement terraform attribute reference

--- terrascript/reference.py
+++ terrascript/reference.py
@@ -2,19 +2,35 @@ from abc import ABC, abstractproperty
 
 
 class ReferenceMixin(ABC):
+
     @abstractproperty
     def ref_list(self):
         ...
 
+    @property
+    def is_var(self):
+        return next(iter(self.ref_list)) == 'var'
+
     def __repr__(self):
-        ref_list = list(self.ref_list)
-        return self.interpolated if ref_list[0] == 'var' else self.fullname
+        return self.interpolated if self.is_var else self.fullname
 
     @property
     def fullname(self):
         return '.'.join(self.ref_list)
 
+    @staticmethod
+    def _interpolate(s):
+        """Interpolated syntax: ``${...}``."""
+        return '${{{}}}'.format(s)
+
     @property
     def interpolated(self):
         """The object in interpolated syntax: ``${...}``."""
-        return '${{{}}}'.format(self.fullname)
+        return self._interpolate(self.fullname)
+
+    def __getattr__(self, item):
+        """Get the terraform attribute of the object,
+        e.g. ``google_compute_address.my_address.ip_version``
+        """
+        s = "{}.{}".format(self.fullname, item)
+        return self._interpolate(s) if self.is_var else s
--- tests/test_15_ReferenceMixin.py
+++ tests/test_15_ReferenceMixin.py
@@ -2,27 +2,29 @@ from terrascript import Resource, Terrascript
 from terrascript.reference import ReferenceMixin
 
 
+class Variable(ReferenceMixin):
+    @property
+    def ref_list(self):
+        return ["var", "foo"]
+
+
+class Data(ReferenceMixin):
+    @property
+    def ref_list(self):
+        return ["data", "foo", "bar"]
+
+
 class Test_ReferenceMixin:
     def setup(self):
         self.config = Terrascript()
 
     def test_reference_mixin(self):
-        class Data(ReferenceMixin):
-            @property
-            def ref_list(self):
-                return ["data", "foo", "bar"]
-
         data = Data()
         assert data.interpolated == "${data.foo.bar}"
         assert data.fullname == "data.foo.bar"
         assert repr(data) == data.fullname
 
     def test_reference_mixin_variable(self):
-        class Variable(ReferenceMixin):
-            @property
-            def ref_list(self):
-                return ["var", "foo"]
-
         var = Variable()
         assert var.interpolated == "${var.foo}"
         assert var.fullname == "var.foo"
@@ -33,3 +35,9 @@ class Test_ReferenceMixin:
         self.config += r
         self.config += Resource("type", "name2", depends_on=[r])
         assert self.config["resource"]["type"]["name2"]["depends_on"] == [repr(r)]
+
+    def test_get_attr(self):
+        assert Data().some_attr == "data.foo.bar.some_attr"
+
+    def test_get_attr_variable(self):
+        assert Variable().some_attr == "${var.foo.some_attr}"
