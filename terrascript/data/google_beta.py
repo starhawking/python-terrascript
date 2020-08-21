72a1941c063d380d1594d2b58de1aad1f0c87410
Ilon Sjögren <ilon.sjogren@enplore.com>
2020-08-21 11:58:01 +0200

    Should support provider google-beta

---
+++ terrascript/data/google_beta.py
@@ -0,0 +1,366 @@
+# terrascript/data/google_beta.py
+import terrascript
+
+
+class google_active_folder(terrascript.Data):
+    pass
+
+
+class google_billing_account(terrascript.Data):
+    pass
+
+
+class google_bigquery_default_service_account(terrascript.Data):
+    pass
+
+
+class google_client_config(terrascript.Data):
+    pass
+
+
+class google_client_openid_userinfo(terrascript.Data):
+    pass
+
+
+class google_cloudfunctions_function(terrascript.Data):
+    pass
+
+
+class google_cloud_identity_groups(terrascript.Data):
+    pass
+
+
+class google_cloud_identity_group_memberships(terrascript.Data):
+    pass
+
+
+class google_composer_image_versions(terrascript.Data):
+    pass
+
+
+class google_compute_address(terrascript.Data):
+    pass
+
+
+class google_compute_backend_service(terrascript.Data):
+    pass
+
+
+class google_compute_backend_bucket(terrascript.Data):
+    pass
+
+
+class google_compute_default_service_account(terrascript.Data):
+    pass
+
+
+class google_compute_forwarding_rule(terrascript.Data):
+    pass
+
+
+class google_compute_global_address(terrascript.Data):
+    pass
+
+
+class google_compute_image(terrascript.Data):
+    pass
+
+
+class google_compute_instance(terrascript.Data):
+    pass
+
+
+class google_compute_instance_serial_port(terrascript.Data):
+    pass
+
+
+class google_compute_instance_group(terrascript.Data):
+    pass
+
+
+class google_compute_lb_ip_ranges(terrascript.Data):
+    pass
+
+
+class google_compute_network(terrascript.Data):
+    pass
+
+
+class google_compute_network_endpoint_group(terrascript.Data):
+    pass
+
+
+class google_compute_node_types(terrascript.Data):
+    pass
+
+
+class google_compute_regions(terrascript.Data):
+    pass
+
+
+class google_compute_region_instance_group(terrascript.Data):
+    pass
+
+
+class google_compute_resource_policy(terrascript.Data):
+    pass
+
+
+class google_compute_router(terrascript.Data):
+    pass
+
+
+class google_compute_ssl_certificate(terrascript.Data):
+    pass
+
+
+class google_compute_ssl_policy(terrascript.Data):
+    pass
+
+
+class google_compute_subnetwork(terrascript.Data):
+    pass
+
+
+class google_compute_vpn_gateway(terrascript.Data):
+    pass
+
+
+class google_compute_zones(terrascript.Data):
+    pass
+
+
+class google_container_cluster(terrascript.Data):
+    pass
+
+
+class google_container_engine_versions(terrascript.Data):
+    pass
+
+
+class google_container_registry_image(terrascript.Data):
+    pass
+
+
+class google_container_registry_repository(terrascript.Data):
+    pass
+
+
+class google_dns_keys(terrascript.Data):
+    pass
+
+
+class google_dns_managed_zone(terrascript.Data):
+    pass
+
+
+class google_game_services_game_server_deployment_rollout(terrascript.Data):
+    pass
+
+
+class google_iam_policy(terrascript.Data):
+    pass
+
+
+class google_iam_role(terrascript.Data):
+    pass
+
+
+class google_iam_testable_permissions(terrascript.Data):
+    pass
+
+
+class google_kms_crypto_key(terrascript.Data):
+    pass
+
+
+class google_kms_crypto_key_version(terrascript.Data):
+    pass
+
+
+class google_kms_key_ring(terrascript.Data):
+    pass
+
+
+class google_kms_secret(terrascript.Data):
+    pass
+
+
+class google_kms_secret_ciphertext(terrascript.Data):
+    pass
+
+
+class google_firebase_web_app(terrascript.Data):
+    pass
+
+
+class google_firebase_web_app_config(terrascript.Data):
+    pass
+
+
+class google_folder(terrascript.Data):
+    pass
+
+
+class google_folder_organization_policy(terrascript.Data):
+    pass
+
+
+class google_monitoring_notification_channel(terrascript.Data):
+    pass
+
+
+class google_monitoring_app_engine_service(terrascript.Data):
+    pass
+
+
+class google_monitoring_uptime_check_ips(terrascript.Data):
+    pass
+
+
+class google_netblock_ip_ranges(terrascript.Data):
+    pass
+
+
+class google_organization(terrascript.Data):
+    pass
+
+
+class google_project(terrascript.Data):
+    pass
+
+
+class google_projects(terrascript.Data):
+    pass
+
+
+class google_project_organization_policy(terrascript.Data):
+    pass
+
+
+class google_secret_manager_secret_version(terrascript.Data):
+    pass
+
+
+class google_service_account(terrascript.Data):
+    pass
+
+
+class google_service_account_access_token(terrascript.Data):
+    pass
+
+
+class google_service_account_id_token(terrascript.Data):
+    pass
+
+
+class google_service_account_key(terrascript.Data):
+    pass
+
+
+class google_sql_ca_certs(terrascript.Data):
+    pass
+
+
+class google_sql_database_instance(terrascript.Data):
+    pass
+
+
+class google_storage_bucket_object(terrascript.Data):
+    pass
+
+
+class google_storage_object_signed_url(terrascript.Data):
+    pass
+
+
+class google_storage_project_service_account(terrascript.Data):
+    pass
+
+
+class google_storage_transfer_project_service_account(terrascript.Data):
+    pass
+
+
+class google_tpu_tensorflow_versions(terrascript.Data):
+    pass
+
+
+class google_redis_instance(terrascript.Data):
+    pass
+
+
+__all__ = [
+    "google_active_folder",
+    "google_billing_account",
+    "google_bigquery_default_service_account",
+    "google_client_config",
+    "google_client_openid_userinfo",
+    "google_cloudfunctions_function",
+    "google_cloud_identity_groups",
+    "google_cloud_identity_group_memberships",
+    "google_composer_image_versions",
+    "google_compute_address",
+    "google_compute_backend_service",
+    "google_compute_backend_bucket",
+    "google_compute_default_service_account",
+    "google_compute_forwarding_rule",
+    "google_compute_global_address",
+    "google_compute_image",
+    "google_compute_instance",
+    "google_compute_instance_serial_port",
+    "google_compute_instance_group",
+    "google_compute_lb_ip_ranges",
+    "google_compute_network",
+    "google_compute_network_endpoint_group",
+    "google_compute_node_types",
+    "google_compute_regions",
+    "google_compute_region_instance_group",
+    "google_compute_resource_policy",
+    "google_compute_router",
+    "google_compute_ssl_certificate",
+    "google_compute_ssl_policy",
+    "google_compute_subnetwork",
+    "google_compute_vpn_gateway",
+    "google_compute_zones",
+    "google_container_cluster",
+    "google_container_engine_versions",
+    "google_container_registry_image",
+    "google_container_registry_repository",
+    "google_dns_keys",
+    "google_dns_managed_zone",
+    "google_game_services_game_server_deployment_rollout",
+    "google_iam_policy",
+    "google_iam_role",
+    "google_iam_testable_permissions",
+    "google_kms_crypto_key",
+    "google_kms_crypto_key_version",
+    "google_kms_key_ring",
+    "google_kms_secret",
+    "google_kms_secret_ciphertext",
+    "google_firebase_web_app",
+    "google_firebase_web_app_config",
+    "google_folder",
+    "google_folder_organization_policy",
+    "google_monitoring_notification_channel",
+    "google_monitoring_app_engine_service",
+    "google_monitoring_uptime_check_ips",
+    "google_netblock_ip_ranges",
+    "google_organization",
+    "google_project",
+    "google_projects",
+    "google_project_organization_policy",
+    "google_secret_manager_secret_version",
+    "google_service_account",
+    "google_service_account_access_token",
+    "google_service_account_id_token",
+    "google_service_account_key",
+    "google_sql_ca_certs",
+    "google_sql_database_instance",
+    "google_storage_bucket_object",
+    "google_storage_object_signed_url",
+    "google_storage_project_service_account",
+    "google_storage_transfer_project_service_account",
+    "google_tpu_tensorflow_versions",
+    "google_redis_instance",
+]
