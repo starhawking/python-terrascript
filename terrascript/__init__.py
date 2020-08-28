f7da611f8f4d1ca2f590186008a6d3ae32dd9cae
Ilon Sjögren <ilon.sjogren@enplore.com>
2020-08-28 16:53:44 +0200

    Should keep spacing and indentation consistent

--- terrascript/__init__.py
+++ terrascript/__init__.py
@@ -1,8 +1,6 @@
 """ terrascript/__init__.py
 
-Base classes and functions that are used everywhere else in
-this project.
-
+Base classes and functions that are used everywhere else in this project.
 """
 
 import json
@@ -31,16 +29,16 @@ TERRAFORM_KEY = "terraform"
 
 
 class Attribute(str):
-    """An `Attribute` handles access to not yet known attributes.
+    """ An `Attribute` handles access to not yet known attributes.
 
-       This called by `Block.__getattr__` to deal with
+        This called by `Block.__getattr__` to deal with
 
-       In the example below the ``aws_instance`` does not have attributes
-       ``.server`` and in turn ``.server.private_ip``. To prevent Python
-       from raising an `AttributeError` the `Attribute.__getattr__()` method
-       creates a new string by appending the attribute name.
+        In the example below the ``aws_instance`` does not have attributes
+        ``.server`` and in turn ``.server.private_ip``. To prevent Python
+        from raising an `AttributeError` the `Attribute.__getattr__()` method
+        creates a new string by appending the attribute name.
 
-       Python:
+        Python:
 
             config = terrascript.Terrascript()
             config += terrascript.aws.aws(version='~> 2.0', region='us-east-1')
@@ -51,7 +49,6 @@ class Attribute(str):
                                                             ^^^^^^^^^^^^^^^^^^
 
         JSON:
-
     """
 
     def __getattr__(self, name):
@@ -59,12 +56,9 @@ class Attribute(str):
 
 
 class Block(dict):
-    """A `Block` is a dictionary-like container for other content.
-
-    """
+    """ A `Block` is a dictionary-like container for other content. """
 
     def __init__(self, **kwargs):
-
         # Convert instances of Resource, Variable, Data, ... into
         # their correct reference instead of inserting the actual
         # dictionary.
@@ -72,7 +66,6 @@ class Block(dict):
         # Resource ->
         # Variable -> "var.name"
         # TODO: Add others?
-        #
         for k, v in kwargs.items():
             if isinstance(v, Variable):
                 kwargs[k] = "var.{}".format(v._name)
@@ -80,29 +73,26 @@ class Block(dict):
         super().__init__(**kwargs)
 
     def __getattr__(self, attr):
-        """Special handling for accessing attributes,
+        """ Special handling for accessing attributes,
 
-           If ``Block.attr`` does not exist, try to return Block[attr]. If that
-           does not exists either, return `attr` as a string, prefixed
-           by the name (and type) of the Block that is referenced.
+            If ``Block.attr`` does not exist, try to return Block[attr]. If that
+            does not exists either, return `attr` as a string, prefixed
+            by the name (and type) of the Block that is referenced.
 
-           This is for example necessary for referencing an attribute of a
-           Terraform resource which only becomes available after the resource
-           has been created.
+            This is for example necessary for referencing an attribute of a
+            Terraform resource which only becomes available after the resource
+            has been created.
 
-           Example:
+            Example:
 
                instance = terrascript.resources.aws_instance("server", ...)
                output = terrascript.Output("instance_ip_addr",
                                            value=instance.private_ip)
                                                         ^^^^^^^^^^
-           Where ``instance.private_ip`` does not (yet) exist.
-
+            Where ``instance.private_ip`` does not (yet) exist.
         """
-
         # Try to return the entry in the dictionary. Otherwise return a string
         # which must be formatted differently depending on what is referenced.
-        #
         if attr in self:
             return self[attr]
         elif attr.startswith("__"):
@@ -135,14 +125,13 @@ class NamedBlock(Block):
 
 
 class NamedSubBlock(Block):
-    """NamedSubBlocks are similar to NamedBlocks except that the `name`
-       is the key to a nested dictionary contain `kwargs`. NamedSubBlocks
-       are not added to the top-level Terrascript structure but are
-       arguments to another block. The backend argument to a Terraform
-       block is a good example. 
+    """ NamedSubBlocks are similar to NamedBlocks except that the `name`
+        is the key to a nested dictionary contain `kwargs`. NamedSubBlocks
+        are not added to the top-level Terrascript structure but are
+        arguments to another block. The backend argument to a Terraform
+        block is a good example.
 
-       :param name: Name of the block, e.g. 'consul'.
-
+    :param name: Name of the block, e.g. 'consul'.
     """
 
     def __init__(self, name, **kwargs):
@@ -151,10 +140,9 @@ class NamedSubBlock(Block):
 
 
 class Terrascript(dict):
-    """Top-level container for Terraform configurations.
+    """ Top-level container for Terraform configurations.
 
-       :param *objects: Optional list of Terrascript data sources, resources,
-
+    :param *objects: Optional list of Terrascript data sources, resources,
     """
 
     def __init__(self, *objects):
@@ -167,8 +155,7 @@ class Terrascript(dict):
         return json.dumps(self, indent=INDENT)
 
     def __add__(self, block):
-        """Add a block to the configuration using the ``+`` syntax."""
-
+        """ Add a block to the configuration using the ``+`` syntax. """
         #
         # Resource
         #
@@ -259,7 +246,7 @@ class Terrascript(dict):
         return self
 
     def add(self, item):
-        """Add to the configuration using the ``+`` syntax."""
+        """ Add to the configuration using the ``+`` syntax. """
         self.__add__(item)
         return item  # for backwards compatibility!
 
@@ -269,7 +256,7 @@ class Terrascript(dict):
             self.add(o)
 
     def __iter__(self):
-        """Iterate over top-level items."""
+        """ Iterate over top-level items. """
 
         def recurse(container):
             if isinstance(container, (Resource, Data, Provider, Variable, Module, Output)):
@@ -289,28 +276,26 @@ class Terrascript(dict):
 
 
 class Resource(NamedBlock):
-    """Terraform resource block."""
-
+    """ Terraform resource block. """
     pass
 
 
 class Data(NamedBlock):
-    """Terraform data source block."""
-
+    """ Terraform data source block. """
     pass
 
 
 class Provider(Block):
-    """Terraform provider
+    """ Terraform provider
 
-       HCL:
+        HCL:
 
             provider "aws" {
                 region = "us-east-1"
                 version = "u~> 2.0"
             }
 
-       JSON:
+        JSON:
 
             "provider": {
                 "aws": [
@@ -320,9 +305,7 @@ class Provider(Block):
                     }
                 ]
             }
-
     """
-
     pass
 
 
@@ -332,12 +315,10 @@ class Variable(NamedBlock):
 
 
 class Module(NamedBlock):
-    """Terraform child module call.
+    """ Terraform child module call.
 
-       https://www.terraform.io/docs/configuration/modules.html
-
+        https://www.terraform.io/docs/configuration/modules.html
     """
-
     pass
 
 
@@ -349,7 +330,7 @@ class Output(NamedBlock):
 
 
 class Provisioner(dict):
-    """A provisioner is a nested dictionary.
+    """ A provisioner is a nested dictionary.
 
        The `name` argument must be a valid Terraform provisioner.
 
@@ -359,7 +340,6 @@ class Provisioner(dict):
        {'local-exec': {'command': "echo 'Hello World"}}
     
        https://www.terraform.io/docs/provisioners/index.html
-
     """
 
     def __init__(self, name, **kwargs):
@@ -388,7 +368,7 @@ class Function(Block):
     pass
 
 
-# Lower case classes for backwards compatability with the exception of 'locals'
+# Lower case classes for backwards compatibility with the exception of 'locals'
 # and the classes that are subclassed in modules (resource, data, provider).
 variable = Variable
 module = Module
