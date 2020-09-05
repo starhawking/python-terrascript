6a7ccf2ad10c8c75e774f7fb7cf5511efa988a22
Hugo <hugovk@users.noreply.github.com>
2020-09-05 22:38:24 +0300

    Don't use fall back to distutils

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 from os.path import join, dirname
 
