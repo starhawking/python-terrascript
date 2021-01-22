64ca42b8ec028b72a672cbe597cc7bbb50fc0736
Ilon Sjögren <ilon@keytrace.se>
2021-01-22 15:25:11 +0100

    Should keep providers formatted with black

--- terrascript/data/signalfx.py
+++ terrascript/data/signalfx.py
@@ -5,11 +5,14 @@ import terrascript
 class signalfx_aws_services(terrascript.Data):
     pass
 
+
 class signalfx_azure_services(terrascript.Data):
     pass
 
+
 class signalfx_dimension_values(terrascript.Data):
     pass
+
 
 class signalfx_gcp_services(terrascript.Data):
     pass
