d64625d9a4a373431eb54578e501110c88baf02c
Ilon Sjögren <ilon@keytrace.se>
2021-01-22 15:21:15 +0100

    Should include python 3.9

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     test_suite="nose.collector",
     tests_require=test_requirements,
